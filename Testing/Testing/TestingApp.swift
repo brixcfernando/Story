dc7d8eab2bcba780866fe6b169119c8179d04d10
Brix Fernando <brixcfernando@gmail.com>
2023-05-12 18:59:37 +0800

    create project

---
+++ Testing/Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Brix on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
